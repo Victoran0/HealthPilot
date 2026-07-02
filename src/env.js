48158190c6924af91678546ba3e570e2f22f296f
Victoran0 <victoranosunkvic@gmail.com>
2026-07-02 14:51:11 +0100

    Add all environment variables

--- src/env.js
+++ src/env.js
@@ -15,6 +15,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    GEMINI_API_KEY: z.string(),
+    GROQ_API_KEY: z.string(),
   },
 
   /**
@@ -34,6 +36,8 @@ export const env = createEnv({
     AUTH_SECRET: process.env.AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    GROQ_API_KEY: process.env.GROQ_API_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
